0b40887ecd111051e343448d37e99bff77cc641d
PhamNhatSang <sang.pn194657@sis.hust.edu.vn>
2023-12-09 09:28:34 +0700

    add search component

--- src/layout/defaultLayout/DefaultLayout.jsx
+++ src/layout/defaultLayout/DefaultLayout.jsx
@@ -1,8 +1,9 @@
 import Footer from "../../components/Footer/Footer";
-
+import Search from "../../components/Search/Search";
 function DefaultLayout({ children }) {
     return (
     <div>
+      <Search/>
       {children}
       <Footer/>
     </div>
