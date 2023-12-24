ff04317307bb71146fe0508fd6d4c495aebe0b16
KeshiNguyen <ducnd06112001@gmail.com>
2023-12-25 00:18:42 +0700

    nothing

--- src/components/HomeListRecipe/index.jsx
+++ src/components/HomeListRecipe/index.jsx
@@ -1,21 +1,22 @@
-// Your component file
-
 import React from 'react';
 
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
-
 import CarouselItem from './CarouselItem';
-  
-function HomeListRecipe({ trendFoods, newFoods, randomFoods }) {
-   
-      return (
+import MostItem from './MostItem/MostItem';
 
-        <div style={{display:'flex',justifyContent:'center',alignItems:'center',flexDirection:'column'}}>
-        <CarouselItem data={trendFoods} title='Xu hướng'></CarouselItem>
-        <CarouselItem data={newFoods} title='Công thức mới nhất'></CarouselItem>
-        <CarouselItem data={randomFoods} title='Hôm nay nấu gì?'></CarouselItem>
-        </div>
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+
+function HomeListRecipe({ trendFoods, newFoods, randomFoods,mostFoods }) {
+      return (
+        <>
+          
+          <div style={{display:'flex',justifyContent:'center',alignItems:'center',flexDirection:'column'}}>
+          <MostItem data={mostFoods} title='Mon an duoc yeu thich nhat'/>
+          <CarouselItem data={trendFoods} title='Xu hướng'></CarouselItem>
+          <CarouselItem data={newFoods} title='Công thức mới nhất'></CarouselItem>
+          <CarouselItem data={randomFoods} title='Hôm nay nấu gì?'></CarouselItem>
+          </div>
+        </>
       );
 }
 
